413496ff8812b1e6bb78cae6c00f0bb3a5eab807
rumangerst <ruman.gerst@gmx.de>
2014-04-25 13:44:11 +0200

    + Implement plotting

--- Übung_2/evolution/PlantEvolution.py
+++ Übung_2/evolution/PlantEvolution.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Created on 24.04.2014
 
@@ -5,7 +7,8 @@ Created on 24.04.2014
 '''
 import random;
 import statistics;
-import itertools;
+import numpy;
+import matplotlib.pyplot as plt;
 
 def clamp(x,_min,_max):
     return min(max(x, _min), _max);
@@ -68,6 +71,12 @@ class Individual:
         print("*-- Individual --*");
         print("u_r(t): " + str(self._growth_bloom)[1:-1]);
         print("R(T) = " + str(self.mass_bloom));
+        
+    #===========================================================================
+    # run after grow()!
+    #===========================================================================
+    def fitness(self):
+        return self.mass_bloom;
     
             
     
@@ -88,6 +97,7 @@ class Individual:
 #===============================================================================
 def evolve(population_size, descendants, r, L_zero, R_zero, T, generations):   
   
+    best_individuals = [];
     population = [Individual(L_zero, R_zero, T) for _ in range(population_size)];
     
     for generation in range(generations):
@@ -102,8 +112,11 @@ def evolve(population_size, descendants, r, L_zero, R_zero, T, generations):
         population.sort(key = lambda x: x.mass_bloom, reverse = True);
         parents = population[0:population_size-1];
         
-        print("Best individual: ");
-        parents[0].printData();
+        #print("Best individual: ");
+        #parents[0].printData();
+        
+        #add to list
+        best_individuals.append(parents[0]);
         
         #create new population of descendants by creating mutants
         population = [];
@@ -111,17 +124,71 @@ def evolve(population_size, descendants, r, L_zero, R_zero, T, generations):
         for x in parents:
             population.extend([x.mutate(L_zero, R_zero, T) for _ in range(descendants)]);
             
-        print("Population size: " + str(len(population)));
+        ##print("Population size: " + str(len(population)));
         
     #Finished! grow, sort and return   
     print("******** Finished ********");
     for individual in population:
         individual.grow(r,T);
     population.sort(key = lambda x: x.mass_bloom, reverse = True);
-    return population[0];
+    best_individuals.append(population[0]);
+
+    return best_individuals;
         
+def plotdata(TEST_DATA, TEST_DATA_u_r, generations, population_size, descendants):
+    
+    
+    data_min = [min(x) for x in TEST_DATA];
+    data_max = [max(x) for x in TEST_DATA];
+    data_avg = [numpy.mean(x) for x in TEST_DATA];
+    data_u_r_avg = [numpy.mean(x) for x in TEST_DATA_u_r];
+    x_axis = range(generations + 1);
+    
+    ############### Create main plot
+    plt.subplot(2,1,1);
+    
+    plt.plot(x_axis, data_min, color='black', linestyle='-', label='Minimum');
+    plt.plot(x_axis, data_avg, color='red', linestyle='-', label='Average');
+    plt.plot(x_axis, data_max, color='black', linestyle='-', label='Maximum');   
         
-        
+    #plt.axis([0,generation])
+    plt.xlabel("Generation ($t$)");
+    plt.ylabel("$R(T)$");
+    plt.title("$R(T)$ über die Generationen $(\mu, \lambda)\ \mu = {0},\ \lambda = {1}$"
+              .format(population_size, descendants));
+    
+    ############### Create u_r plot
+    plt.subplot(2,1,2);
+    
+    plt.xlabel("Generation ($t$)");
+    plt.ylabel("$u_r$");
+    plt.title("$u_r$ über die Generation");
+    
+    plt.plot(x_axis, data_u_r_avg, color='red', linestyle=':', label='Average');
+    
+    
+    #show
+    plt.show(); 
+    
+def main_readandplot():
+    
+    file = open("testdata_params", "r");    
+    data_params = file.read();
+    data_params = eval(data_params);    
+    file.close();
+    
+    file = open("testdata", "r");    
+    data = file.read();
+    data = eval(data);    
+    file.close();
+    
+    file = open("testdata_u_r", "r");    
+    data_u_r = file.read();
+    data_u_r = eval(data_u_r);
+    file.close();
+    
+    plotdata(data, data_u_r, data_params[6], data_params[0], data_params[1]);
+    
     
 def main():
     population_size = 10;
@@ -130,10 +197,52 @@ def main():
     L_zero = 0.01;
     R_zero = 0;
     T = 20;
-    generations = 100;
+    generations = 200;
     
     best = evolve(population_size, descendants, energy_production_factor, L_zero, R_zero, T, generations);
+    best = best[generations]; #generations are 0 - <generations> (parents are counting as gen 0)
     best.printData();
     
+def main_plottest():
+    population_size = 10;
+    descendants = 3;
+    energy_production_factor = 0.1;
+    L_zero = 0.01;
+    R_zero = 0;
+    T = 20;
+    generations = 200;
     
-main();
+    TEST_RUNS = 25;
+    TEST_DATA_PARAMS = [population_size, descendants, energy_production_factor, L_zero, R_zero, T, generations];
+    TEST_DATA = [[] for _ in range(generations + 1)];
+    TEST_DATA_u_r = [[] for _ in range(generations + 1)];
+    
+    for run in range(TEST_RUNS):
+        print("------------- * Test Run " + str(run));
+        
+        best_individuals = evolve(population_size, descendants, energy_production_factor, L_zero, R_zero, T, generations);
+                
+        for generation in range(generations + 1):
+            TEST_DATA[generation].append( best_individuals[generation].mass_bloom );
+            TEST_DATA_u_r[generation].append( best_individuals[generation].growth_bloom(T - 1) );
+        
+    #first, write testdata to file for eventual later use
+    file = open("testdata", "w");
+    file.write(str(TEST_DATA));
+    file.close();
+    
+    file = open("testdata_u_r", "w");
+    file.write(str(TEST_DATA_u_r));
+    file.close();
+    
+    file = open("testdata_params", "w");
+    file.write(str(TEST_DATA_PARAMS));
+    file.close();
+    
+    #ok, plot testdata
+    plotdata(TEST_DATA, TEST_DATA_u_r, generations, population_size, descendants);
+    
+    
+    
+#main_plottest();
+main_readandplot();
